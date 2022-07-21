68be0f18def4ea87a4738081e18f71412cea1a3f
joschne <jonas.schneider@balcab.ch>
2022-07-21 10:11:47 +0200

    add react server side data fetch and SSE support

--- packages/react-output-target/react-component-lib/createComponent.tsx
+++ packages/react-output-target/react-component-lib/createComponent.tsx
@@ -1,5 +1,5 @@
-import React, { createElement } from 'react';
-
+import React, { createElement, useEffect } from 'react';
+import { useSSE } from './utils/useSSE';
 import {
   attachProps,
   camelToDashCase,
@@ -18,9 +18,14 @@ interface StencilReactInternalProps<ElementType> extends React.HTMLAttributes<El
   ref?: React.Ref<any>;
 }
 
+interface HTMLStencilFetchElement extends HTMLStencilElement {
+  fetchData?: () => Promise<any>;
+  data?: any;
+}
+
 export const createReactComponent = <
   PropType,
-  ElementType extends HTMLStencilElement,
+  ElementType extends HTMLStencilFetchElement,
   ContextStateType = {},
   ExpandedPropsTypes = {}
 >(
@@ -37,71 +42,71 @@ export const createReactComponent = <
   }
 
   const displayName = dashToPascalCase(tagName);
-  const ReactComponent = class extends React.Component<StencilReactInternalProps<ElementType>> {
-    componentEl!: ElementType;
+  const ReactComponent = (props: StencilReactInternalProps<ElementType>) => {
+    let componentEl!: ElementType;
 
-    setComponentElRef = (element: ElementType) => {
-      this.componentEl = element;
+    const setComponentElRef = (element: ElementType) => {
+      componentEl = element;
+    };
+    useEffect(() => {
+      attachProps(componentEl, props, props);
+    });
+
+    useSSE(async () => {
+
+      // we don't want to fetch here, if we are in a client
+      if(typeof window) return;
+
+      await customElements.whenDefined(tagName);
+      if (componentEl?.fetchData) {
+        const data = await componentEl.fetchData();
+
+        attachProps(componentEl, { ...props, data }, props);;
+        return data;
+      }
+      return;
+    });
+
+    const { children, forwardedRef, style, className, ref, ...cProps } = props;
+
+    let propsToPass = Object.keys(cProps).reduce((acc: any, name) => {
+      const value = (cProps as any)[name];
+
+      if (name.indexOf('on') === 0 && name[2] === name[2].toUpperCase()) {
+        const eventName = name.substring(2).toLowerCase();
+        if (typeof document !== 'undefined' && isCoveredByReact(eventName)) {
+          acc[name] = value;
+        }
+      } else {
+        // we should only render strings, booleans, and numbers as attrs in html.
+        // objects, functions, arrays etc get synced via properties on mount.
+        const type = typeof value;
+
+        if (type === 'string' || type === 'boolean' || type === 'number') {
+          acc[camelToDashCase(name)] = value;
+        }
+      }
+      return acc;
+    }, {});
+
+    if (manipulatePropsFunction) {
+      propsToPass = manipulatePropsFunction(props, propsToPass);
+    }
+
+    const newProps: Omit<StencilReactInternalProps<ElementType>, 'forwardedRef'> = {
+      ...propsToPass,
+      ref: mergeRefs(forwardedRef, setComponentElRef),
+      style,
     };
 
-    constructor(props: StencilReactInternalProps<ElementType>) {
-      super(props);
-    }
-
-    componentDidMount() {
-      this.componentDidUpdate(this.props);
-    }
-
-    componentDidUpdate(prevProps: StencilReactInternalProps<ElementType>) {
-      attachProps(this.componentEl, this.props, prevProps);
-    }
-
-    render() {
-      const { children, forwardedRef, style, className, ref, ...cProps } = this.props;
-
-      let propsToPass = Object.keys(cProps).reduce((acc: any, name) => {
-        const value = (cProps as any)[name];
-
-        if (name.indexOf('on') === 0 && name[2] === name[2].toUpperCase()) {
-          const eventName = name.substring(2).toLowerCase();
-          if (typeof document !== 'undefined' && isCoveredByReact(eventName)) {
-            acc[name] = value;
-          }
-        } else {
-          // we should only render strings, booleans, and numbers as attrs in html.
-          // objects, functions, arrays etc get synced via properties on mount.
-          const type = typeof value;
-
-          if (type === 'string' || type === 'boolean' || type === 'number') {
-            acc[camelToDashCase(name)] = value;
-          }
-        }
-        return acc;
-      }, {});
-
-      if (manipulatePropsFunction) {
-        propsToPass = manipulatePropsFunction(this.props, propsToPass);
-      }
-
-      const newProps: Omit<StencilReactInternalProps<ElementType>, 'forwardedRef'> = {
-        ...propsToPass,
-        ref: mergeRefs(forwardedRef, this.setComponentElRef),
-        style,
-      };
-
-      /**
-       * We use createElement here instead of
-       * React.createElement to work around a
-       * bug in Vite (https://github.com/vitejs/vite/issues/6104).
-       * React.createElement causes all elements to be rendered
-       * as <tagname> instead of the actual Web Component.
-       */
-      return createElement(tagName, newProps, children);
-    }
-
-    static get displayName() {
-      return displayName;
-    }
+    /**
+     * We use createElement here instead of
+     * React.createElement to work around a
+     * bug in Vite (https://github.com/vitejs/vite/issues/6104).
+     * React.createElement causes all elements to be rendered
+     * as <tagname> instead of the actual Web Component.
+     */
+    return createElement(tagName, newProps, children);
   };
 
   // If context was passed to createReactComponent then conditionally add it to the Component Class
