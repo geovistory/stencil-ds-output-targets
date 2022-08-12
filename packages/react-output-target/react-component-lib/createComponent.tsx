8311909aa333cbaecd8a93acd67430bf9104200b
joschne <jonas.schneider@balcab.ch>
2022-08-12 14:59:31 +0200

    cleaning

--- packages/react-output-target/react-component-lib/createComponent.tsx
+++ packages/react-output-target/react-component-lib/createComponent.tsx
@@ -5,7 +5,7 @@ import {
   createForwardRef,
   dashToPascalCase,
   isCoveredByReact,
-  mergeRefs
+  mergeRefs,
 } from './utils';
 import { StencilSSRFunction } from './utils/serverRenderWebComponent';
 import { useSSE } from './utils/useSSE';
@@ -14,19 +14,11 @@ export interface HTMLStencilElement extends HTMLElement {
   componentOnReady(): Promise<this>;
 }
 
-interface StencilReactInternalProps<ElementType>
-  extends React.HTMLAttributes<ElementType> {
+interface StencilReactInternalProps<ElementType> extends React.HTMLAttributes<ElementType> {
   forwardedRef: React.RefObject<ElementType>;
   ref?: React.Ref<any>;
 }
-type CProps<ElementType> = Omit<
-  StencilReactInternalProps<ElementType>,
-  'children' | 'forwardedRef' | 'style' | 'className' | 'ref'
->;
-export type NewProps<ElementType> = Omit<
-  StencilReactInternalProps<ElementType>,
-  'forwardedRef'
->;
+export type NewProps<ElementType> = Omit<StencilReactInternalProps<ElementType>, 'forwardedRef'>;
 export interface HTMLStencilFetchElement extends HTMLStencilElement {
   fetchData?: () => Promise<any>;
   data?: any;
@@ -42,11 +34,11 @@ export const createReactComponent = <
   ReactComponentContext?: React.Context<ContextStateType>,
   manipulatePropsFunction?: (
     originalProps: StencilReactInternalProps<ElementType>,
-    propsToPass: any
+    propsToPass: any,
   ) => ExpandedPropsTypes,
   defineCustomElement?: () => void,
   componentClass?: any,
-  stencilRenderToString?: StencilSSRFunction
+  stencilRenderToString?: StencilSSRFunction,
 ) => {
   if (defineCustomElement !== undefined) {
     defineCustomElement();
@@ -100,13 +92,12 @@ export const createReactComponent = <
      */
     const [sse, error] = useSSE(async () => {
       // stop, if we are in a browser
-      if (!isServer || !componentClass || !stencilRenderToString) return;
+      if (!isServer || !componentClass || !stencilRenderToString) return;
 
       let data: any;
 
       // instantiate new component
-      displayName;
-
+      componentClass.__attachShadow = () => {}; // shim __attachShadow()
       const component = new componentClass();
 
       // assign properties to the component
@@ -116,21 +107,21 @@ export const createReactComponent = <
       if (component?.fetchData) {
         data = await component.fetchData();
       }
-      const { serverRenderWebComponent } = await import(
-        './utils/serverRenderWebComponent'
-      );
 
       // render webcomponent html (using stencil hydrate)
+      const { serverRenderWebComponent } = await import('./utils/serverRenderWebComponent');
       const html = await serverRenderWebComponent<ElementType>(
         tagName,
         newProps,
         children,
         stencilRenderToString,
-        data
+        data,
       );
 
       return { data, html };
     });
+
+    console.warn(error)
 
     /**
      * We use createElement here instead of
