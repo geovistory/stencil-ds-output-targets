150ae568f7fdd0b124b431024dbea87e7daeeda2
joschne <jonas.schneider@balcab.ch>
2022-08-18 16:27:19 +0200

    improve shims

--- packages/react-output-target/react-component-lib/utils/shimHtmlElement.ts
+++ packages/react-output-target/react-component-lib/utils/shimHtmlElement.ts
@@ -1,39 +1,39 @@
 function shimHtmlElement(element: any) {
   element.prototype.__attachShadow = () => { }; // shim __attachShadow()
   element.prototype.attachShadow = () => { }; // shim attachShadow()
-  element.prototype.__createElement = (nodeName) => { };
-  element.prototype.createElement = (nodeName) => { };
-  element.prototype.__createTextNode = (value) => { };
-  element.prototype.createTextNode = (value) => { };
-  element.prototype.__appendChild = (node) => { };
-  element.prototype.appendChild = (node) => { };
-  element.prototype.__replaceChild = (node) => { };
-  element.prototype.replaceChild = (node) => { };
-  element.prototype.__removeChild = (node) => { };
-  element.prototype.removeChild = (node) => { };
-  element.prototype.__insertBefore = (newItem, existing) => { };
-  element.prototype.insertBefore = (newItem, existing) => { };
+  element.prototype.__createElement = () => { };
+  element.prototype.createElement = () => { };
+  element.prototype.__createTextNode = () => { };
+  element.prototype.createTextNode = () => { };
+  element.prototype.__appendChild = () => { };
+  element.prototype.appendChild = () => { };
+  element.prototype.__replaceChild = () => { };
+  element.prototype.replaceChild = () => { };
+  element.prototype.__removeChild = () => { };
+  element.prototype.removeChild = () => { };
+  element.prototype.__insertBefore = () => { };
+  element.prototype.insertBefore = () => { };
   element.prototype.__toString = () => { };
   element.prototype.toString = () => { };
-  element.prototype.__setAttribute = (name, value) => { };
-  element.prototype.setAttribute = (name, value) => { };
-  element.prototype.__getAttribute = (name) => { };
-  element.prototype.getAttribute = (name) => { };
-  element.prototype.__setProperty = (name, value) => { };
-  element.prototype.setProperty = (name, value) => { };
-  element.prototype.__getProperty = (name) => { };
-  element.prototype.getProperty = (name) => { };
+  element.prototype.__setAttribute = () => { };
+  element.prototype.setAttribute = () => { };
+  element.prototype.__getAttribute = () => { };
+  element.prototype.getAttribute = () => { };
+  element.prototype.__setProperty = () => { };
+  element.prototype.setProperty = () => { };
+  element.prototype.__getProperty = () => { };
+  element.prototype.getProperty = () => { };
   element.prototype.__innerHTML = () => { };
   element.prototype.innerHTML = () => { };
   element.prototype.__outerHTML = () => { };
   element.prototype.outerHTML = () => { };
   element.prototype.__textContent = () => { };
   element.prototype.textContent = () => { };
-  element.prototype.__addEventListener = (eventType, listenerFunc) => { };
-  element.prototype.addEventListener = (eventType, listenerFunc) => { };
-  element.prototype.__removeEventListener = (eventType, listenerFunc) => { };
-  element.prototype.removeEventListener = (eventType, listenerFunc) => { };
-  element.prototype.__dispatchEvent = (event) => { };
-  element.prototype.dispatchEvent = (event) => { };
+  element.prototype.__addEventListener = () => { };
+  element.prototype.addEventListener = () => { };
+  element.prototype.__removeEventListener = () => { };
+  element.prototype.removeEventListener = () => { };
+  element.prototype.__dispatchEvent = () => { };
+  element.prototype.dispatchEvent = () => { };
   return element
 }
