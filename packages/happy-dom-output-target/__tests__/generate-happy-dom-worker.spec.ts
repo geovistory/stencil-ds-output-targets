a9f8133fa0777e5c6f23ac14cae98b3e7028f41c
joschne <jonas.schneider@balcab.ch>
2022-08-31 14:06:09 +0200

    wait for lazy loading stuff

--- packages/happy-dom-output-target/__tests__/generate-happy-dom-worker.spec.ts
+++ packages/happy-dom-output-target/__tests__/generate-happy-dom-worker.spec.ts
@@ -38,44 +38,47 @@ document.write(\`
 let renderId = 0;
 
 const start = async () => {
-    const { defineCustomElements } = await import('@my-company/my-components/loader')
-    defineCustomElements(undefined)
+  const { defineCustomElements } = await import('@my-company/my-components/loader')
+  defineCustomElements()
 
-    // render web component(s) to html, including data fetching
-    const document = globalThis.document;
-    var div = document.createElement('div');
-    div.setAttribute('class', 'happy-dom-wrapper');
-    const id = 'happy-dom-wrapper-' + renderId;
-    renderId++;
-    div.setAttribute('id', id);
-    div.setAttribute('class', 'happy-dom-wrapper');
-    div.innerHTML = workerData.html.trim();
-    document.body.appendChild(div)
+  // wait one tick to wait for lazyLoaded stencil components
+  await new Promise(res =>setTimeout(()=>{res(true)},100))
+
+  // render web component(s) to html, including data fetching
+  const document = globalThis.document;
+  var div = document.createElement('div');
+  div.setAttribute('class', 'happy-dom-wrapper');
+  const id = 'happy-dom-wrapper-' + renderId;
+  renderId++;
+  div.setAttribute('id', id);
+  div.setAttribute('class', 'happy-dom-wrapper');
+  div.innerHTML = workerData.html.trim();
+  document.body.appendChild(div)
 
 
-    const wrapper = window?.document?.body?.querySelector('#' + id);
+  const wrapper = window?.document?.body?.querySelector('#' + id);
 
-    // wait one tick to wait for lazyLoaded stencil components
-    await new Promise(res =>setTimeout(()=>{res(true)},0))
-    return window.happyDOM.whenAsyncComplete().then(() => {
-        document.body.removeChild(div)
-        // @ts-ignore
-        const data = wrapper.data
-        // console.log(data);
+  // wait one tick to let components add async tasks
+  await new Promise(res =>setTimeout(()=>{res(true)},0))
 
-        // Do something when all async tasks are completed.
-        const html = wrapper?.getInnerHTML({ includeShadowRoots: true })
-        // console.log(html);
-        return { data, html }
-    });
+  return window.happyDOM.whenAsyncComplete().then(() => {
+      document.body.removeChild(div)
+      // @ts-ignore
+      const data = wrapper.data
+      // console.log(data);
+
+      // Do something when all async tasks are completed.
+      const html = wrapper?.getInnerHTML({ includeShadowRoots: true })
+      // console.log(html);
+      return { data, html }
+  });
 
 
 }
 
 start().then((res) => {
-    parentPort?.postMessage({ success: res });
-}).catch(e => parentPort?.postMessage({ error: e }))
-`)
+  parentPort?.postMessage({ success: res });
+}).catch(e => parentPort?.postMessage({ error: e }))`)
   });
 
 });
